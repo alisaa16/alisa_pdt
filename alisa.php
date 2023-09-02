d939d1873469f16fea7ad93226d21658ab63e305
alisaa16 <143861392+alisaa16@users.noreply.github.com>
2023-09-02 11:56:37 +0700

    Add files via upload

---
+++ alisa.php
@@ -0,0 +1,11 @@
+<?php
+// Deklarasi variabel
+$nama = "Alisa Nur Fadilah";
+$kelas = "B";
+$npm = "217006040";
+
+// Menampilkan perkenalan diri
+echo "Nama saya " . $nama . ".<br>";
+echo "Saya dari kelas " . $kelas . ".<br>";
+echo "NPM saya adalah " . $npm . ".<br>";
+?>
